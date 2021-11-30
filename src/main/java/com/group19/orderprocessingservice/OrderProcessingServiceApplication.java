3bee3a6773dd2725192ecb84e33560d93426677e
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2021-11-30 12:13:09 +0000

    implemented eureka client

--- src/main/java/com/group19/orderprocessingservice/OrderProcessingServiceApplication.java
+++ src/main/java/com/group19/orderprocessingservice/OrderProcessingServiceApplication.java
@@ -2,8 +2,10 @@ package com.group19.orderprocessingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderProcessingServiceApplication {
 
 	public static void main(String[] args) {
